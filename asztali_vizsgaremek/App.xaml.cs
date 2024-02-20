0fd6c49ca25c9cbc76ae65d9d2f38e8092bf42c9
Csontos <csontostibor48@gmail.com>
2024-02-20 11:39:44 +0100

    Add project files.

---
+++ asztali_vizsgaremek/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace asztali_vizsgaremek
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
