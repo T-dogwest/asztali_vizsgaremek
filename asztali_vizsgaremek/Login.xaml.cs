f267e211fd2ef21e24a8ad081303d8ab1717aae3
Csontos <csontostibor48@gmail.com>
2024-02-26 13:52:00 +0100

    logint

--- asztali_vizsgaremek/Login.xaml.cs
+++ asztali_vizsgaremek/Login.xaml.cs
@@ -14,9 +14,9 @@ namespace asztali_vizsgaremek
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Login : Window
     {
-        public MainWindow()
+        public Login()
         {
             InitializeComponent();
         }
